7aac7440894b1468c3981c41d857d2632fc8898f
Prash <prashman55@hotmail.com>
2014-03-01 16:09:04 +0000

    error handling and send method

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
-from flask import Flask
+from flask import Flask, make_response
 import json
 
 app = Flask("test_app")
+
+
+def _send(data, err=None):
+	"""Merge output with default data."""
+	
+	dict_ = {"version":"0.1",
+			"err":err,
+			"data":data}
+	resp = make_response(json.dumps(dict_))
+	resp.headers['Content-Type'] ='application/json'
+	return resp
+	
+@app.errorhandler(404)
+def error(msg):
+	err = "Error. {0}".format(msg)
+	return _send([], err=err)
 
 @app.route("/login/", methods=['POST', 'GET'])
 def login():
@@ -11,17 +27,16 @@ def login():
 @app.route("/docs/", methods=['GET'])
 def docs():
 	app.logger.info("hello")
-	return "a collection of docs"
+	return _send([])
 
 @app.route("/doc/<int:docid>/", methods=['GET', 'POST'])
 def doc(docid):
-	#app.logger.info("doc hello")
-	dict_ = {'id':docid,
+	"""Return a doc for classification."""
+	
+	data = {'id':docid,
 			'text':"Some made up text for classification"}
-	doc = json.dumps(dict_)
-	return doc
-	#return "a test doc with id: {0}".format(docid)
-
+	return _send(data)
+	
 	
 if __name__ == "__main__":
 	app.run(debug=True)

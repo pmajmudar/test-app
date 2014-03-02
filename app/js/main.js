8d31a8036ac8de2c41baeb96fc58b958b30e772d
Prash <prashman55@hotmail.com>
2014-03-02 23:57:48 +0000

    some tidying

--- app/js/main.js
+++ app/js/main.js
@@ -19,16 +19,23 @@ function($, _, Backbone, Model){
 	// Create router with allowed routes
 	var AppRouter = Backbone.Router.extend({
 		routes:{
-			'search':'search',
+			'classify/:id':'classify',
 			'*pages':'defaultRoute' /*match any uncaught urls */
 		}
 	});
 	
 	var app_router = new AppRouter;
 
-	app_router.on('route:search', function(){
-		var anything;
-		console.log("I'm searching!");
+	app_router.on('route:classify', function(id){
+		console.log(id);
+		console.log("I'm classifying!");
+		var m = new Model({id:id});
+		console.log(m.url());
+
+		m.fetch({ success: function (m) {
+					console.log(m);
+				}
+		});
 	});
 	
 	app_router.on('route:defaultRoute', function(pages){
@@ -37,8 +44,7 @@ function($, _, Backbone, Model){
 	});
 	
 
-	var m = new Model;
-	console.log(m);
+	
 	
 	Backbone.history.start({pushState: true});
 });

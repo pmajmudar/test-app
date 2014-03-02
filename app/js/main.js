1bc69e973b0d52fd1a35d55b02d723b05f0c2394
Prash <prashman55@hotmail.com>
2014-03-02 12:32:05 +0000

    added router

--- app/js/main.js
+++ app/js/main.js
@@ -3,22 +3,38 @@
 requirejs.config({
 	// Default load from js/lib
 	baseUrl: 'app/js/libs',
-	// if it starts with app then use the app dir.
+	// if it starts with src then use the src dir.
 	// This is relative to the baseurl
 	paths:{
-		app:'../src',
+		src:'../src',
 		underscore:'underscore-min',
-		backbone:'backbone-min'
+		backbone:'backbone-min',
+		jquery:'jquery-1.11.0.min'
 	}
 });
 
-requirejs(['underscore', 'backbone', 'app/model'],
-function(_, Backbone, model){
-	// app model is loaded?
+requirejs(['jquery', 'underscore', 'backbone', 'src/model'],
+function($, _, Backbone, model){
+	
+	// Create router with allowed routes
 	var AppRouter = Backbone.Router.extend({
 		routes:{
-			'/search':
+			'search':'search',
+			'*pages':'defaultRoute' /*match any uncaught urls */
 		}
 	});
 	
+	var app_router = new AppRouter;
+
+	app_router.on('route:search', function(){
+		var anything;
+		console.log("I'm searching!");
+	});
+	
+	app_router.on('route:defaultRoute', function(pages){
+		console.log("In default");
+		console.log(pages);
+	});
+	
+	Backbone.history.start({pushState: true});
 });
